21aedf12cd0b9c95b6431e2d99995cba370729dc
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-17 17:48:27 +0530

    [Aniket]- Perfrom Types of constuctor in c#

---
+++ PracticeProblems/Constructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+        class ConstructorCar
+    {
+        string carModel;
+        int carYear;
+        public ConstructorCar(string Model, int Year) //This is Parameterised Constuctor
+        {
+            carModel = Model;
+            carYear = Year;
+            Console.WriteLine("Mode:-" + carModel);
+            Console.WriteLine("Year:-" + carYear);
+        }
+    }
+}
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
+        public Program() //This Is User Defined Constuctor //Define Constuctor Name Same As Class Name
+        {
+            Console.WriteLine("Hello, I am Constuctor");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Guys!!! Performs Constructor ,Interface, Access Modifier");
+            Program obj = new Program();//This is Defalut Constuctor
+            ConstructorCar constructorCar = new ConstructorCar("BMW", 1998);
+            
         }
     }
 }
