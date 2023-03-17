ccbf96ac98110ba2f8000ac29d81d6df1a7e4369
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-17 18:10:25 +0530

    [Aniket]- Perfrom interface in c#

---
+++ Interface/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IAnimal
+    {
+        void animalSound(); // interface method (does not have a body)
+    }
+    class Pig : IAnimal
+    {
+        public void animalSound()
+        {
+          
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+}
